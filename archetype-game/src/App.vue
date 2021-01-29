15011d1b6b1ae27786faaffe6bb17ab64f3ccf96
OverAngelix <OverAngelix@hotmail.fr>
2021-01-29 23:58:54 +0100

    FEATURE : Ajout de la V1

---
+++ archetype-game/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <Accueil msg="Mon application" />
+  </div>
+</template>
+
+<script>
+import Accueil from "./components/Accueil.vue";
+
+export default {
+  name: "App",
+  components: {
+    Accueil,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
